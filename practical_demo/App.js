cdda7c0f99bc19734f85b6809a2cc3cdd7301767
M Malysa <mariastud2023@gmail.com>
2025-05-22 21:19:16 +0300

    -

--- practical_demo/App.js
+++ practical_demo/App.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('hello world!')
-})
+    res.send('Hello World!');
+});
 
 app.get('/feature-1', (req, res) => {
-    res.send('Feature 1!')
-})
+    res.send('Feature 1!');
+});
+
+app.get('/feature-2', (req, res) => {
+    res.send('Feature 2!');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
